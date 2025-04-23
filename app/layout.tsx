4ebaaddf67422f6e713e17ed37099ab62d828e72
Vivek Shankar Phadake <129990463+Vivek20sp@users.noreply.github.com>
2025-04-23 15:19:38 +0530

    Third Commit

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Vivek Shankar Phadake",
+  description:
+    "Explore the portfolio of Vivek Shankar Phadake, a proficient Full Stack Developer skilled in React.js, TypeScript, Node.js, and MongoDB. Dive into his innovative projects, professional experience, and technical expertise in building scalable web applications and modern interfaces.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.png" sizes="any" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ data/index.ts
@@ -0,0 +1,257 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      "Prioritizing collaboration and open communication to deliver tailored client solutions.",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm adaptable with time zone communication.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Social Media Analytics Site",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "NFT Web3 Online Multiplayer Card Game",
+    des: "Engineered a blockchain-based multiplayer card game featuring NFT ownership on Avalanche, with React.js frontend and Solidity smart contracts for secure asset trading and gameplay mechanics.",
+    img: "/nft_card_game.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg","/ether.svg",'/hardhat.svg','/solidity.svg'],
+    link: "nft-web3-card-game.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Live Collaborator",
+    des: "Developed an intuitive collaborative platform enabling multiple users to create and edit documents simultaneously with real-time synchronization and conflict resolution.",
+    img: "/live_collaborator.svg",
+    iconLists: ["/re.svg",'nextjs.svg',"/tail.svg", "/ts.svg", "/liveblocks.svg",'lexical_editor.svg','/clerk.svg'],
+    link: "https://live-docs-two-gray.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Social Media Application",
+    des: "Experience a seamless and modern social media platform with stunning design and smooth interactions, powered by  TypeScript",
+    img: "/Social_Media.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/appwrite_cloud.svg"],
+    link: "https://ritiverse.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Orbi E-Commerce Store",
+    des: "Simplify your shopping experience with ORBI. Seamlessly browse, buy, and pay with ease.",
+    img: "/Orbi_E-Commerce.svg",
+    iconLists: [
+      "/re.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/MongoDBLogo.svg",
+      "/RazorPay.svg",
+    ],
+    link: "https://main--orbi-e-mart.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Startup Hub Platform",
+    des: "A dynamic platform showcasing innovative startups, powered by cutting-edge technology and AI-driven features.",
+    img: "/Start_up_application.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/Sanity-logo.svg"],
+    link: "https://yc-directory-jxk4.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Audio Electronics E-Commerce Store",
+    des: "Crafted a premium Audio Electronics E-commerce store, blending smooth FRAMER animations with immersive 3D effects, and integrated a seamless payment gateway.",
+    img: "/Audio_E-Commerce.svg",
+    iconLists: [
+      "/re.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/MongoDBLogo.svg",
+      "/RazorPay.svg",
+      "/Framer.svg",
+    ],
+    link: "https://audio-e-commerce-mern-main.netlify.app/#/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Vivek Shankar Phadake was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Vivek Shankar Phadake's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Vivek Shankar Phadake is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Vivek Shankar Phadake was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Vivek Shankar Phadake's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Vivek Shankar Phadake is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Vivek Shankar Phadake was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Vivek Shankar Phadake's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Vivek Shankar Phadake is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Vivek Shankar Phadake was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Vivek Shankar Phadake's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Vivek Shankar Phadake is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Vivek Shankar Phadake was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Vivek Shankar Phadake's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Vivek Shankar Phadake is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "netlify",
+    img: "/netlify.svg",
+    nameImg: "/netlify-logo.svg",
+  },
+  {
+    id: 4,
+    name: "framer",
+    img: "/Framer.svg",
+    nameImg: "/Framer-logo.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Front End Developer Intern - Cubiccode Digital Media LLP",
+    desc: "I engineered responsive React/TypeScript applications, transforming Figma designs into pixel-perfect UI components. Working in an agile environment, I collaborated across teams to optimize frontend architecture and deliver client-focused solutions that met all technical requirements.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+  {
+    id: 2,
+    title: "Full Stack Developer Intern - Goloka IT",
+    desc: "Collaborated with the team to transform Figma designs into responsive web applications, developed front-end features with ReactJS, maintained back-end with Node.js, Express.js, and MongoDB, and optimized performance by troubleshooting and refining the codebase.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 3,
+    title: "Mobile App Dev - Perfect E-Learning",
+    desc: "Translated Figma designs into functional Android applications with Android Studio and Java, developed intuitive front-end features, integrated back-end systems using SQLite and Firebase, collaborated with clients for feedback, and optimized code for enhanced performance",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 4,
+    title: "Mobile App Developer - Oasis Infobyte",
+    desc: "Converted Figma prototypes into operational Android applications utilizing Android Studio and Java, designing user-friendly front-end elements and integrating back-end systems with SQLite.Focused on optimizing code for better efficiency, speed, and dependability.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  // {
+  //   id: 4,
+  //   title: "Lead Frontend Developer",
+  //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+  //   className: "md:col-span-2",
+  //   thumbnail: "/exp4.svg",
+  // },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Vivek20sp",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://x.com/vi9840449035",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/vivek-shankar-phadake-2b6216218/",
+  },
+];
